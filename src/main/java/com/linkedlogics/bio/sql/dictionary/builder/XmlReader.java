2031659c09a2091a262938b50ec43980bbc782c5
radjab <radjab@gmail.com>
2019-04-29 18:55:01 +0400

    java 11 changes

--- src/main/java/com/linkedlogics/bio/sql/dictionary/builder/XmlReader.java
+++ src/main/java/com/linkedlogics/bio/sql/dictionary/builder/XmlReader.java
@@ -1,6 +1,7 @@
 package com.linkedlogics.bio.sql.dictionary.builder;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.sql.Types;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,16 @@
 /**
+ * 
+ */
+/**
  * @author rajab
  *
  */
 module com.linkedlogics.bio.sql {
 	exports com.linkedlogics.bio.sql;
-	exports com.linkedlogics.bio.sql.annotation;
-	
-	requires transitive com.linkedlogics.bio ;
-	requires transitive java.sql;
+
+	requires com.linkedlogics.bio;
+	requires io.github.classgraph;
+	requires java.sql;
+	requires java.xml;
+	requires org.json;
 }
